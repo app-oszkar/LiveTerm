c68f5ec0a83abe759aa23667bd548487bd6067af
KiwiChat <52504536+KiwiChat@users.noreply.github.com>
2023-11-03 14:21:50 +0100

    Update Ps1.tsx

--- src/components/Ps1.tsx
+++ src/components/Ps1.tsx
@@ -4,11 +4,11 @@ import config from '../../config.json';
 export const Ps1 = () => {
   return (
     <div>
-      <span className="text-light-yellow dark:text-dark-yellow">
+      <span className="text-light-white dark:text-dark-blue">
         {config.ps1_username}
       </span>
       <span className="text-light-gray dark:text-dark-gray">@</span>
-      <span className="text-light-green dark:text-dark-green">
+      <span className="text-light-yellow dark:text-dark-green">
         {config.ps1_hostname}
       </span>
       <span className="text-light-gray dark:text-dark-gray">:$ ~ </span>
